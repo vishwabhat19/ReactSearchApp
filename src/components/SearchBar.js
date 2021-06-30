65bc6fdb1493163da4dc3bf6ff813b56c07584b6
Vishwa <VB086617>
2021-07-01 01:37:36 +0530

    Code for calling parent component function from child component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,8 @@ class SearchBar extends React.Component {
 
     onFormSubmit = (event) => {
         event.preventDefault();
-        console.log(this.state.term);
+        this.props.onSearchSubmit(this.state.term);
+        
     }
 
     render() {
